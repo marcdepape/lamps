7894b192889a4f5871069a07ca0447dd3bc54654
Marc De Pape <marc.de.pape@gmail.com>
2016-10-30 14:23:52 -0400

    Check for stream connection success

--- rpi_lamp_behaviour/stream_control.py
+++ rpi_lamp_behaviour/stream_control.py
@@ -13,50 +13,51 @@ class AudioStream(object):
         self.playing = False
         self.volume = 0.0
 
-        bus = self.stream.get_bus()
-        bus.add_signal_watch()
-        bus.connect("message", self.on_message)
+    def state(self, play):
+        self.playing = play.get()
+        return self.playing
 
-    def on_message(self, bus, message):
-        t = message.type
-        if t == Gst.MessageType.EOS:
-            self.stream.set_state(Gst.State.NULL)
-            print message
-        elif t == Gst.MessageType.ERROR:
-            self.stream.set_state(Gst.State.NULL)
-            err, debug = message.parse_error()
-            print "Error: %s" % err, debug
-
-    def fade(self, q, inOut):
+    def fade(self, vol, inOut):
         if inOut == "in":
             print "FADE IN!"
-            print "CURRENT VOLUME: " + str(self.volume)
+            #print "CURRENT VOLUME: " + str(self.volume)
             while self.volume < 1.50:
                 self.volume = self.volume + 0.01
-                print str(self.volume)
-                q.put(self.volume)
+                #print str(self.volume)
+                vol.put(self.volume)
                 sleep(0.05)
         elif inOut == "out":
             print "FADE OUT"
-            print "CURRENT VOLUME: " + str(self.volume)
+            #print "CURRENT VOLUME: " + str(self.volume)
             while self.volume > 0.01:
                 self.volume = self.volume - 0.01
-                print str(self.volume)
-                q.put(self.volume)
+                #print str(self.volume)
+                vol.put(self.volume)
                 sleep(0.05)
         else:
             pass
 
-    def listen(self, q, url):
+    def listen(self, vol, play, url):
         print "PLAYING! " + url
-
-        self.playing = True
         self.volume = 0
         self.stream.get_by_name("source").set_property("location",url)
         self.stream.get_by_name("gain").set_property('volume', self.volume)
-        self.stream.set_state(Gst.State.PLAYING)
+
+        while self.playing == False:
+            self.stream.get_by_name("source").set_property("location",url)
+            self.stream.set_state(Gst.State.PLAYING)
+            sleep(5)
+            info = self.stream.get_state(0)
+            info = str(info[0]).split(" ")
+            print info[1]
+            if info[1] == "GST_STATE_CHANGE_SUCCESS":
+                self.playing = True
+                play.put(self.playing)
+            else:
+                self.stream.set_state(Gst.State.NULL)
+
         while self.playing == True:
-            self.volume = q.get()
+            self.volume = vol.get()
             if self.volume != self.stream.get_by_name("gain").get_property('volume'):
                 self.stream.get_by_name("gain").set_property('volume', self.volume)
             else:
@@ -66,47 +67,26 @@ GObject.threads_init()
 Gst.init(None)
 
 mainloop = GObject.MainLoop()
-Thread(name="mainloop", target=mainloop.run).start()
+m = Thread(name="mainloop", target=mainloop.run)
+m.daemon = True
+m.start()
 
 this_stream = AudioStream()
 
 if __name__ == '__main__':
-    while True:
-        q = Queue()
-        t1 = Process(name='listen1', target=this_stream.listen, args=(q, "rtsp://192.168.100.162:8554/mic",))
-        print "START!"
-        t1.start()
-        sleep(5)
-        print "VOLUME!"
-        this_stream.fade(q, "in")
-        sleep(5)
-        this_stream.fade(q, "out")
-        sleep(1)
-        print "STOP!"
-        t1.terminate()
-
-        q = Queue()
-        t1 = Process(name='listen1', target=this_stream.listen, args=(q, "rtsp://192.168.100.117:8554/mic",))
-        print "START!"
-        t1.start()
-        sleep(5)
-        print "VOLUME!"
-        this_stream.fade(q, "in")
-        sleep(5)
-        this_stream.fade(q, "out")
-        sleep(1)
-        print "STOP!"
-        t1.terminate()
-
-        q = Queue()
-        t1 = Process(name='listen1', target=this_stream.listen, args=(q, "rtsp://192.168.100.189:8554/mic",))
-        print "START!"
-        t1.start()
-        sleep(5)
-        print "VOLUME!"
-        this_stream.fade(q, "in")
-        sleep(5)
-        this_stream.fade(q, "out")
-        sleep(1)
-        print "STOP!"
-        t1.terminate()
+    vol = Queue()
+    play = Queue()
+    t1 = Process(name='listen1', target=this_stream.listen, args=(vol, play, "rtsp://192.168.100.200:8554/mic",))
+    print "START!"
+    t1.start()
+    sleep(5)
+    print "SUCCESS?"
+    while this_stream.state(play) == False:
+        pass
+    print "FADE IN!"
+    this_stream.fade(vol, "in")
+    sleep(5)
+    this_stream.fade(vol, "out")
+    sleep(1)
+    print "STOP!"
+    t1.terminate()
